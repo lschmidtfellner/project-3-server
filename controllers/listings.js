1d8b152ac9ce1fdf225ef08e1f6e427ecb648b3d
genpai99 <darioussanchez@gmail.com>
2023-06-20 10:59:58 -0400

    Darious Model update, controller logic updated

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,3 +1,4 @@
+const CarListing = require('../models/CarListing');
 const SalePost = require('../models/CarListings');
 
 // Create a new sale post
@@ -6,10 +7,10 @@ async function createSalePost(req, res) {
     // Retrieve the sale post data from the request body
     const postData = req.body;
     // Create a new SalePost instance using the SalePost model
-    const salePost = new SalePost(postData);
+    const salePost = new CarListing(postData);
     // Save the sale post to the database
     await salePost.save();
-    res.status(201).json(salePost);
+    res.status(201).json(carListing);
   } catch (error) {
     res.status(500).json({ error: 'Failed to create sale post' });
   }
@@ -21,11 +22,11 @@ async function getSalePostById(req, res) {
     // Retrieve the sale post ID from the request parameters
     const { id } = req.params;
     // Find the sale post in the database by its ID
-    const salePost = await SalePost.findById(id);
+    const salePost = await CarListing.findById(id);
     if (!salePost) {
       res.status(404).json({ error: 'Sale post not found' });
     } else {
-      res.json(salePost);
+      res.json(carListing);
     }
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve sale post' });
@@ -36,8 +37,8 @@ async function getSalePostById(req, res) {
 async function getAllSalePosts(req, res) {
   try {
     // Retrieve all sale posts from the database
-    const salePosts = await SalePost.find();
-    res.json(salePosts);
+    const salePosts = await CarListing.find();
+    res.json(carListings);
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve sale posts' });
   }
@@ -51,7 +52,7 @@ async function updateSalePost(req, res) {
     // Retrieve the updated sale post data from the request body
     const updatedData = req.body;
     // Find the sale post in the database by its ID and update it
-    const salePost = await SalePost.findByIdAndUpdate(id, updatedData, { new: true });
+    const carListing = await CarListing.findByIdAndUpdate(id, updatedData, { new: true });
     if (!salePost) {
       res.status(404).json({ error: 'Sale post not found' });
     } else {
@@ -68,7 +69,7 @@ async function deleteSalePost(req, res) {
     // Retrieve the sale post ID from the request parameters
     const { id } = req.params;
     // Find the sale post in the database by its ID and remove it
-    const salePost = await SalePost.findByIdAndRemove(id);
+    const salePost = await CarListing.findByIdAndRemove(id);
     if (!salePost) {
       res.status(404).json({ error: 'Sale post not found' });
     } else {
--- models/CarListing.js
+++ models/CarListing.js
@@ -25,6 +25,7 @@ const carListingSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
+        required: true,
     }
 });
 
