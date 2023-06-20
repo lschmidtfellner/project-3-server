48b269661a33b2e8b7d9823336e57f2405262779
granth2023 <grantharris38@gmail.com>
2023-06-20 14:42:35 -0400

    model capital

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import User from '../models/User.js';
 //import verifyAuth from '../middlewares/verifyAuth.js';
 
 dotenv.config();
