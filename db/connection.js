26ca34a8d0d439be79361f1e48b2dd869152b2f7
granth2023 <grantharris38@gmail.com>
2023-06-20 14:44:40 -0400

    error message

--- db/connection.js
+++ db/connection.js
@@ -58,7 +58,7 @@ mongoose.connect(MONGODB_URI, mongooseConfig)
 
 mongoose.connection.on('connected', () => {console.log('Connected to database!')})
 mongoose.connection.on('disonnected', () => {console.log('Disconnected to database!')})
-mongoose.connection.on('error', () => {console.log('Error connecting to database! Look out for: ', error)})
+mongoose.connection.on('error', (error) => {console.log('Error connecting to database! Look out for: ', error)})
 
 
 export default mongoose.connection 
