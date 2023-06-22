9be67264dae64d6dc0e86a020805cd8b6b3dc66b
lschmidtfellner <lschmidtfellner@gmail.com>
2023-06-22 11:28:47 -0400

    added user in sign in response

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,61 +1,59 @@
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
 //import verifyAuth from '../middlewares/verifyAuth.js';
 
-dotenv.config();
+dotenv.config()
 
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = process.env.SECRET_KEY
 
 async function signup(req, res) {
-    console.log(req.body)
-    try {
-      const { username, password, email } = req.body;
-  
-      console.log('Received signup request:', { username, email });
-  
-      // Validate that the password field is not empty or undefined
-      if (!password) {
-        console.log('Invalid password:', password);
-        return res.status(400).json({ error: "Password is required" });
-      }
-        
-      let user = await User.findOne({ email });
-      if (user) {
-        console.log('Email already exists:', email);
-        return res.status(400).json({ error: "Email already exists" });
-      }
-  
-      const saltRounds = 10; // Set the desired number of salt rounds
-      const hash = await bcrypt.hash(password, saltRounds);
-  
-      console.log('Creating user:', { username, email });
-  
-      user = await User.create({
-        username,
-        password: hash,
-        email,
-      });
-  
-      console.log('User created:', user);
-  
-      return res.status(200).json({
-        status: 200,
-        message: `Successfully created user: ${user.username}`,
-        success: true
-      });
-    } catch (error) {
-      console.error('Error during signup:', error);
-      res.status(400).json({
-        status: 400,
-        error: "Unable to create user",
-        database_message: error.message,
-      });
-    }
-  }
-  
+  console.log(req.body)
+  try {
+    const { username, password, email } = req.body
 
+    console.log('Received signup request:', { username, email })
+
+    // Validate that the password field is not empty or undefined
+    if (!password) {
+      console.log('Invalid password:', password)
+      return res.status(400).json({ error: 'Password is required' })
+    }
+
+    let user = await User.findOne({ email })
+    if (user) {
+      console.log('Email already exists:', email)
+      return res.status(400).json({ error: 'Email already exists' })
+    }
+
+    const saltRounds = 10 // Set the desired number of salt rounds
+    const hash = await bcrypt.hash(password, saltRounds)
+
+    console.log('Creating user:', { username, email })
+
+    user = await User.create({
+      username,
+      password: hash,
+      email
+    })
+
+    console.log('User created:', user)
+
+    return res.status(200).json({
+      status: 200,
+      message: `Successfully created user: ${user.username}`,
+      success: true
+    })
+  } catch (error) {
+    console.error('Error during signup:', error)
+    res.status(400).json({
+      status: 400,
+      error: 'Unable to create user',
+      database_message: error.message
+    })
+  }
+}
 
 async function signin(req, res) {
   try {
@@ -72,88 +70,86 @@ async function signin(req, res) {
 
     const data = {
       id: user._id,
-      username: user.username,
+      username: user.username
     }
 
-    const token = jwt.sign(data, SECRET_KEY);
+    const token = jwt.sign(data, SECRET_KEY)
 
-    res.cookie('token', token, { httpOnly: true });
+    res.cookie('token', token, { httpOnly: true })
 
     return res.status(200).json({
       status: 200,
       message: `Successfully signed in ${user.username}`,
-      token: token
+      token: token,
+      user: user
     })
   } catch (error) {
     res.status(404).json({
       status: 404,
       error: `User not found`,
-      database_message: error.message,
+      database_message: error.message
     })
   }
 }
 
 async function isTokenValid(req, res) {
-    try {
-      if (req.id) {
-        res.status(200).json({
-          valid: true,
-          status: 200,
-          message: "Token is valid"
-        })
-      }
-    } catch (error) {
-      res.status(400).json({
-        valid: false,
-        status: 400,
-        error: `Token is invalid`,
-        database_message: error.message,
+  try {
+    if (req.id) {
+      res.status(200).json({
+        valid: true,
+        status: 200,
+        message: 'Token is valid'
       })
     }
+  } catch (error) {
+    res.status(400).json({
+      valid: false,
+      status: 400,
+      error: `Token is invalid`,
+      database_message: error.message
+    })
   }
-  
+}
 
 async function updateUsername(req, res) {
   try {
     const { newUsername } = req.body
     const user = await User.findOne({ username: newUsername })
-    
+
     if (user) {
-      return res.status(400).json({ error: "Username already exists" });
+      return res.status(400).json({ error: 'Username already exists' })
     }
 
-    await User.findByIdAndUpdate(req.user._id, { username: newUsername });
-    
+    await User.findByIdAndUpdate(req.user._id, { username: newUsername })
+
     return res.status(200).json({
       status: 200,
       message: `Successfully updated username to: ${newUsername}`
-    }) 
-
+    })
   } catch (error) {
     res.status(400).json({
       status: 400,
       error: `Unable to update username`,
-      database_message: error.message,
+      database_message: error.message
     })
   }
 }
 
 async function deleteUsername(req, res) {
   try {
-    await User.findByIdAndUpdate(req.user._id, { username: null });
-    
+    await User.findByIdAndUpdate(req.user._id, { username: null })
+
     return res.status(200).json({
       status: 200,
       message: `Successfully deleted username`
-    }) 
-
+    })
   } catch (error) {
     res.status(400).json({
       status: 400,
       error: `Unable to delete username`,
-      database_message: error.message,
+      database_message: error.message
     })
   }
 }
 
-export { isTokenValid, signup, signin, updateUsername, deleteUsername };
+export { isTokenValid, signup, signin, updateUsername, deleteUsername }
