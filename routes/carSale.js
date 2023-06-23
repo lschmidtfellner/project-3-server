150c41226f678b8bdd1c0076da908effe9124762
genpai99 <darioussanchez@gmail.com>
2023-06-23 13:51:44 -0400

    Test

--- routes/carSale.js
+++ routes/carSale.js
@@ -5,7 +5,7 @@ import { upload } from '../controllers/upload.js';
 const router = express.Router();
 
 // Sale post routes
-router.post('/saleposts', upload.array('images', 5), salePostController.createSalePost);
+router.post('/saleposts', salePostController.createSalePost);
 router.get('/saleposts/:id', salePostController.getSalePostById);
 router.get('/saleposts', salePostController.getAllSalePosts);
 router.put('/saleposts/:id', salePostController.updateSalePost);
